62841bbd2d2025a2be20e7c4813e01e05be9e754
EllieWalsch <elliewalsch4@gmail.com>
2022-11-08 11:05:03 -0600

    Compare id in each time block to current hour

--- Develop/script.js
+++ Develop/script.js
@@ -9,13 +9,26 @@ let year = date.getFullYear();
 let currentDate = `${month}/${day}/${year}`;
 currentDayEl.text(currentDate);
 
+let hour = date.getHours();
 
+// Compare element ID to current hour
+function changeBackground () {
+  $(".time-block").each(function () {
+    // turns ID string into a number
+    let hourBlock = parseInt($(this).attr("id"));
+    console.log(hourBlock);
+  });
+  console.log(hour);
+};
+
+changeBackground ();
+
+// TODO: compare id in each time block to hour
 
 
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-$(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -33,5 +46,3 @@ $(function () {
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
   //
-  // TODO: Add code to display the current date in the header of the page.
-});
